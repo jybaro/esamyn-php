9414aa743afd3cae53934ec01e1897ed441f7acb
Edgar Valarezo <edgar.valarezo@gmail.com>
2017-11-28 12:10:00 -0500

    Config BDD en array, Respaldos con config, respaldos de app, ajustes a respaldos

--- public/respaldos.php
+++ public/respaldos.php
@@ -1,9 +1,37 @@
+<h1> Respaldos </h1>
 <?php
+
 if (isset($_POST['respaldar']) && !empty($_POST['respaldar'])) {
 
-    $nombre = date('Ymd-His');
-    exec('export PGPASSWORD="esamYn.2017" && pg_dump --file "'.$nombre.'.backup" --host "45.79.192.236" --port "5432" --username "esamyn_user" --no-password --verbose --role "esamyn_user" --format=c --blobs "acess"');
-    echo "<a class='btn btn-success' href='/$nombre.backup'><span class='glyphicon glyphicon-download-alt' aria-hidden='true'></span> Descargar respaldo generado</a>";
+    $respaldar = $_POST['respaldar'];
+
+    if ($respaldar == 'bdd') {
+        $nombre = 'esamyn-bdd-'. date('Ymd-His') . '.backup';
+        $comando = ('export PGPASSWORD="'.$bdd_config['password'].'" && pg_dump --file "'.$nombre.'" --host "'.$bdd_config['host'].'" --port "'.$bdd_config['port'].'" --username "'.$bdd_config['user'].'" --no-password --verbose --role "'.$bdd_config['user'].'" --format=c --blobs "'.$bdd_config['dbname'].'"');
+        //echo $comando;
+        exec($comando . ' 2>&1', $output);
+
+        if (file_exists($nombre) && filesize($nombre) > 100) {
+            $size = round(filesize($nombre) / (1024 * 1024));
+            echo "<div class='alert alert-success'><h3>Respaldo de la base de datos generado con éxito</h3>";
+            echo "<a class='btn btn-warning' href='/$nombre'><span class='glyphicon glyphicon-download-alt' aria-hidden='true'></span> Descargar $nombre ($size MB)</a>";
+            echo "</div>";
+        } else {
+            echo "<div class='alert alert-danger'>Hubo un error al generar el respaldo de la base de datos.</div>";
+        }
+    } else if ($respaldar == 'app') {
+        sleep(4);
+        $nombre = 'esamyn-maven-master.zip';
+        if (file_exists($nombre) && filesize($nombre) > 100) {
+            $size = round(filesize($nombre) / (1024 * 1024));
+            echo "<div class='alert alert-success'><h3>Respaldo del aplicativo generado con éxito</h3>";
+            echo "<a class='btn btn-warning' href='/$nombre'><span class='glyphicon glyphicon-download-alt' aria-hidden='true'></span> Descargar $nombre ($size MB)</a>";
+            echo "</div>";
+        } else {
+            echo "<div class='alert alert-danger'>Hubo un error al generar el respaldo del aplicativo</div>";
+        }
+    }
+    echo "<hr>";
 }
 
 
@@ -11,6 +39,11 @@ if (isset($_POST['respaldar']) && !empty($_POST['respaldar'])) {
 
 
 <form method="POST">
-<input type="hidden" name="respaldar" value="respaldar">
-<button class="btn btn-info"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Generar Respaldo</button>
+<input type="hidden" name="respaldar" value="bdd">
+<button class="btn btn-info"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Generar respaldo de la base de datos</button>
+</form>
+
+<form method="POST">
+<input type="hidden" name="respaldar" value="app">
+<button class="btn btn-info"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Generar respaldo del aplicativo</button>
 </form>
